0b5f267524f6281166d6ab924cff8d529891c093
Juan Pablo Solano <juanpa_solano@hotmail.com>
2017-06-23 13:34:04 -0500

    mongo

--- src/server/index.js
+++ src/server/index.js
@@ -19,8 +19,7 @@ const shopifyToken = new ShopifyToken({
 
 //Mongodb/Mongoose
 const mongoDb = 
-  process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
+  process.env.MONGODB_URI ||
   'mongodb://localhost/shopify-app';
 
 mongoose.Promise = global.Promise;
