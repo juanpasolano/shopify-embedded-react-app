fc7c33cc4b56702cd03bec231a8fef3229c3e483
Juan Pablo Solano <juanpa_solano@hotmail.com>
2017-06-25 20:50:31 -0500

    cloud

--- src/client/routes.js
+++ src/client/routes.js
@@ -7,7 +7,7 @@ class Routes extends Component {
   render() {
     return (
       <BrowserRouter >
-        <EmbeddedApp {...window.shopSession} forceRedirect={true}>
+        <EmbeddedApp {...window.shopSession} forceRedirect={false}>
           <div>
             <Route exact path='/' component={App} />
           </div>
